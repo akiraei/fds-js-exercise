3a585cf2a1c2766d00948d8f8694a70e9fd778fc
akiraei <akiraeiphoto@gmail.com>
2018-04-10 17:16:02 +0900

    exercise

--- 1-number.js
+++ 1-number.js
@@ -178,20 +178,50 @@ console.log(destructor(240));
 
 
 
-
-
-
 // ### 문제 7
 
 // 2 이상의 자연수를 입력받아, 그 수가 소수인지 아닌지를 판별하는 함수를 작성하세요.
 
+let prime_list = [2];
+const l = prime_list.length
 
 
+function prime (x) {
+  let t = 0;
+  if( x <= 2 || Math.floor(x) != x ){
+    if (x == 2) {
+      return "prime"
+    } else{return "wrong input";}
+      } else {
 
+    // console.log("right input");
+    for (let i = 0; i < l; i++) {
+      let n = x % prime_list[i];
+      if (n === 0 ) {
+        return "no prime"
+      }
+     }
+    //  console.log("out list, enter wild")
+     for (let j = prime_list[l]; j < x; j++){
+          // console.log("after for, begin wild");
+          let k = x % j;
+          if (k === 0 ) {
+            return "no prime";
+          }}
+     prime_list.push(x);
+     console.log(prime_list);
+     return "prime";
+  }
+}
 
-
-
-
+ function prime_collector (x) {
+   prime_list = [2];
+   for (let i = 3; i < x; i++) {
+     prime (i);
+   }
+ }
+  
+  
 
 
 
@@ -199,6 +229,24 @@ console.log(destructor(240));
 // ### 문제 8
 
 // 1부터 100까지의 수를 차례대로 출력하되, 자릿수에 3, 6, 9중 하나라도 포함되어 있으면 '짝!'을 대신 출력하는 프로그램을 작성하세요.
+
+let list_369 = [];
+
+function check_369 (x) {
+for (let i = 1; i < x+1; i++) {
+  let str = i.toString();
+  if( str.includes('3') || str.includes('6')  || str.includes('9') ) {
+    list_369.push("짝");
+  } else { list_369.push(i)}
+}
+return list_369;
+}
+
+
+
+
+
+
 
 // ### 문제 9
 
@@ -224,6 +272,29 @@ console.log(destructor(240));
 // * * * *
 // * * * * *
 // ```
+
+
+
+function piramid (x) {
+  let str = "\n";
+  for (let i = 1; i < x + 1; i++) {
+    let str_2 = "*".repeat(i)+"\n";
+    str = str + str_2
+  }
+  return str;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 // ### 문제 10
 
@@ -256,14 +327,84 @@ console.log(destructor(240));
 //     *
 // ```
 
+
+
+
+
+
+
 // ### 문제 11
 
 // 두 수를 입력받아서, 두 수의 최대공약수를 반환하는 함수를 작성하세요. ([유클리드 호제법](https://ko.wikipedia.org/wiki/%EC%9C%A0%ED%81%B4%EB%A6%AC%EB%93%9C_%ED%98%B8%EC%A0%9C%EB%B2%95)을 참고하세요.)
+
+
+
+
+
+
+
+
+
+
+
 
 // ### 문제 12
 
 // 세 수를 입력받아 큰 것부터 차례대로 출력하는 함수를 작성하세요.
 
+
+let ls = [1,3,4,6,9,7,11,-3,56,34,2,5,3,453,32,112,5435]
+
+function compare_list (ls) {
+  let l = ls.length;
+  let t = 0;
+  for (let i = 0; i < l; i++) {
+    if (ls[l-i-1] < ls[l-i]){
+      let buffer = ls[l-i-1];
+      ls[l-i-1] = ls[l-i];
+      ls[l-i] = buffer;
+      t += 1;    
+    } 
+  }
+  console.log("t",t)
+  if (t === 0) {
+    return ls;
+  } else {
+    compare_list(ls);
+  }
+  return ls;
+}
+
+compare_list(ls);
+
+
+
+
+
+
+
+
 // ### 문제 13
 
 // 자연수 `n`을 입력받아, `n`번째 피보나치 수를 반환하는 함수를 작성하세요.
+
+
+function pipi (n) {
+
+  let list = [1,1]
+  for (let i = 0; i < n -2; i++){
+    list[i+2] = list [i+1] + list[i]
+  }
+  return list[list.length-1]
+}
+
+pipi (99)
+
+// 내 풀이. 지저분 하다. 죽것음
+
+
+function fibonacci(num) { 
+  if(num < 2) return num;
+   return fibonacci(num-1) + fibonacci(num-2); }
+
+ //  남의 재귀함수 풀이. 겁나 아름답다.
